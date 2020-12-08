4767496b4428107a05958fba760bfa4bf10be5b0
sejin <3jin.pk@gmail.com>
2020-12-08 11:44:06 +0900

    웹훅테스트

--- src/main/java/docswave/test/sj/SjApplication.java
+++ src/main/java/docswave/test/sj/SjApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SjApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SjApplication.class, args);
 	}
-
 }
